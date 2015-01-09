9106f2715bd5ef80b961137341fa42d29a3921a3
juanramonestrada <jrtecest@gmail.com>
2015-01-08 23:10:42 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: juan
+ * Date: 21/10/14
+ * Time: 10:29 PM
+ */
+
+public class Demo {
+    private $url = "";
+    private $id = 1;
+    public function __construct(){
+        $this->id = 1;
+
+    }
+}
+?>
